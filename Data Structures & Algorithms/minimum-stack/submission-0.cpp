95287ac3d16c103c4208b9a354ab93c3775d48be
Mayank Upadhyay <131614752+upadhyay1302@users.noreply.github.com>
2026-07-28 20:58:27 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+public:
+    vector<int> st;
+    vector<int> minSt;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push_back(val);
+        if(minSt.empty() || minSt.back() >= val){
+            minSt.push_back(val);
+        }
+        else{
+            minSt.push_back(minSt.back());
+        }
+    }
+    
+    void pop() {
+        st.pop_back();
+        minSt.pop_back();
+    }
+    
+    int top() {
+        return st.back();
+    }
+    
+    int getMin() {
+        return minSt.back();
+    }
+};
