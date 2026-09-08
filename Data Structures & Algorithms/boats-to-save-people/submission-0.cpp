ccc0bf0088f9d57f26f1dc3af08b6928e2c7acce
Mayank Upadhyay <131614752+upadhyay1302@users.noreply.github.com>
2026-09-08 17:54:44 -0400

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int boats= 0;
+        int start = 0;
+        int end = people.size() - 1;
+
+        while(start <= end){
+            if(people[start] + people[end] <= limit){
+                start++;
+                end--;
+            }
+            else end --;
+            boats++;
+        }
+        return boats;
+
+    }
+};
