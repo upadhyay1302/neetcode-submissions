2b9c6843088430c357da84352a5fd9cc7c6100ab
Mayank Upadhyay <131614752+upadhyay1302@users.noreply.github.com>
2026-09-14 14:22:35 -0400

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+
+        int i = 0;
+
+        // intervals overlap if 
+        // s1,e1 and s2,e2
+        // e2 >= s1 && e1 >= s2 for exaple [1,3] and [2,5] 
+
+        while(i < intervals.size() && intervals[i][1] < newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        while(i < intervals.size() && (intervals[i][1] >= newInterval[0] && newInterval[1] >= intervals[i][0])){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1],intervals[i][1] );
+            i++;
+        }
+
+        ans.push_back(newInterval);
+
+        while(i < intervals.size()){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
+    }
+};
